a8c5a5ff026ecf720d46431df39132fd1becb2e7
Miguel Castro <mcastro_23@alu.uabcs.mx>
2026-05-10 18:59:20 -0700

    Optimizar configuración de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,64 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// Base config that applies to either development or production mode.
-const config = {
-  entry: './src/index.js',
-  output: {
-    // Compile the source files into a bundle.
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  // Enable webpack-dev-server to get hot refresh of the app.
-  devServer: {
-    static: './build',
-  },
-  module: {
-    rules: [
-      {
-        // Load CSS files. They can be imported into JS files.
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    // Generate the HTML index page based on our template.
-    // This will output the same index page with the bundle we
-    // created above added in a script tag.
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-    }),
-  ],
-  resolve: {
-    fallback: {
-      "vm": require.resolve('vm-browserify'),
-    },
-  },
-};
-
 module.exports = (env, argv) => {
-  if (argv.mode === 'development') {
-    // Set the output path to the `build` directory
-    // so we don't clobber production builds.
-    config.output.path = path.resolve(__dirname, 'build');
+  const isProduction = argv.mode === 'production';
 
-    // Generate source maps for our code for easier debugging.
-    // Not suitable for production builds. If you want source maps in
-    // production, choose a different one from https://webpack.js.org/configuration/devtool
-    config.devtool = 'eval-cheap-module-source-map';
-
-    // Include the source maps for Blockly for easier debugging Blockly code.
-    config.module.rules.push({
-      test: /(blockly[/\\].*\.js)$/,
-      use: [require.resolve('source-map-loader')],
-      enforce: 'pre',
-    });
-
-    // Ignore spurious warnings from source-map-loader
-    // It can't find source maps for some Closure modules and that is expected
-    config.ignoreWarnings = [/Failed to parse source map.*blockly/];
-  }
-  return config;
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, isProduction ? 'dist' : 'build'),
+      filename: isProduction ? '[name].[contenthash].js' : '[name].bundle.js',
+      clean: true,
+    },
+    devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    devServer: {
+      static: path.resolve(__dirname, 'build'),
+      hot: true,
+      compress: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /(blockly[/\\].*\.js)$/,
+          use: [require.resolve('source-map-loader')],
+          enforce: 'pre',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        minify: isProduction ? {
+          removeComments: true,
+          collapseWhitespace: true,
+          minifyJS: true,
+          minifyCSS: true,
+        } : false,
+      }),
+    ],
+    resolve: {
+      fallback: {
+        "vm": require.resolve('vm-browserify'),
+      },
+    },
+    optimization: {
+      moduleIds: 'deterministic',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+        },
+      },
+    },
+    ignoreWarnings: [/Failed to parse source map.*blockly/],
+  };
 };
